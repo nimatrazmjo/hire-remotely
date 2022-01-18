281a45746141102d2cf32786a53c101b2587466d
Nimatullah Razmjo <nimat.razmjo@gmail.com>
2022-01-18 09:49:04 +0430

    Add codemirror and tailwindcss page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}
