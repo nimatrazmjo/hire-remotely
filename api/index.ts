49676f640a849e38837034daa5f3d66dc88536f1
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-02-13 17:19:46 +0430

    Add mongodb with persisten volume k8s

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,13 @@
 import http from 'http';
+import { connect } from 'mongoose';
 
 import { app } from './src/app'
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 const start = async () => {
+    await connect(`mongodb://mongo-service:27017/nimat`); 
+    console.log('Connected to mongo DB');
   server.listen(PORT,()=>{
     console.info('api is listening on port', PORT);
   });
