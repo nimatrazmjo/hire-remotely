9cf82e008cfddaa3d6fd612e7515f7a8a16e97a1
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-03-19 23:47:26 +0430

    Remove unnecessary field from test interface and collection

--- api/src/models/test.ts
+++ api/src/models/test.ts
@@ -1,66 +1,15 @@
 
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 import { ITestCase } from 'src/interfaces/test/test-case.interface';
 import { ITest } from 'src/interfaces/test/test.interface';
 
-export interface TestDocument extends ITest, Document { }
 
-export interface TestCaseDocument extends ITestCase, Document { }
-
-const TestCaseSchema = new mongoose.Schema<TestCaseDocument>({
-  name: {
-    type: String,
-    required: true
-  },
-  input: {
-    type: String,
-    required: true
-  },
-  expectedOutput: {
-    type: String,
-    required: true
-  },
-  status: {
-    id: {
-      type: Number, // 3: Accepted, 4: Wrong answer
-      required: true,
-    },
-    description: {
-      type: String, // Accepted, Wrong Answer
-      required: true
-    }
-  },
-  time: {
-    type: String,
-    required: true
-  },
-  memory: {
-    type: String,
-    required: true
-  },
-  givenOutput: String
-});
-
-const TestSchema = new mongoose.Schema<TestDocument>({
+const TestSchema = new mongoose.Schema<ITest, Model<ITest>>({
   hash: {
     type: String,
     required: true
   },
-  question: {
-    type: String,
-    required: true
-  },
-  result: {
-    language_id: {
-      type: Number,
-      required: true
-    },
-    answer: {
-      type: Number,
-      required: true
-    },
-    testCase: [TestCaseSchema]
-  }
+  questions: [String]
  
 },{
   toJSON:{
@@ -70,6 +19,6 @@ const TestSchema = new mongoose.Schema<TestDocument>({
   }
 });
 
-const Test = mongoose.model<TestDocument>('Test', TestSchema);
+const Test = mongoose.model('Test', TestSchema);
 
 export default Test;
