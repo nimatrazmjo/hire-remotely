dd4a443a6948f51c2aedb101aca5d52908dac9b7
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-03-24 14:54:11 +0430

    Fix layout isue

--- client/pages/[hash].js
+++ client/pages/[hash].js
@@ -7,14 +7,15 @@ import Question from '../components/question/question.component';
 import Answer from '../components/answer/answer.component';
 
 const Test = () => {
-    const router = useRouter()
-    const { hash } = router.query
-    return (
-        <div className='bg-white flex-grow'>
-        <div className='mx-10 h-full'>
+  const router = useRouter()
+  const { hash } = router.query
+  return (
+    <div className=' bg-white flex-grow'>
+      <div className='container flex mx-auto'>
+        <div className='h-full'>
           <div className='grid grid-cols-2 h-full'>
             <div className='bg-gray-100 h-full p-5'>
-                <Question />
+              <Question />
             </div>
             <div className='h-full p-4'>
               <Answer />
@@ -24,7 +25,8 @@ const Test = () => {
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Test
