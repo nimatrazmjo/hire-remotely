f96b2b2a4ec24a236ee4ca4b1c36ab96d3464341
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-04-03 17:18:47 +0430

    add loading to router to route based

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -12,6 +12,24 @@ import "nprogress/nprogress.css";
 import TopNavComponent from '../components/top-nav/top-nav.component';
 
 const MyApp = ({ Component, pageProps }) => {
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
+
   return (
     <div className='h-screen flex flex-col'>
       <Provider store={store}>
