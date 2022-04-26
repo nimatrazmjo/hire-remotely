6abc4b604c665558194afdec53f77b52fc8f9d5b
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-04-26 21:33:04 +0430

    Ehance language request

--- client/components/custom-select/custom-select.component.jsx
+++ client/components/custom-select/custom-select.component.jsx
@@ -7,13 +7,15 @@ const CustomButton = ({ setLanguageId }) => {
   const [languages, setLanguages] = useState([]);
   const {test} = useSelector(state => state.test);
   const snippetLanguage = test?.languages;
-  useEffect(async () => {
-    const { data } = await axios.get('https://ce.judge0.com/languages');
-    setLanguages(data);
+  useEffect(() => {
+    const fetchLanguage = async () => {
+      const { data } = await axios.get('https://ce.judge0.com/languages');
+      setLanguages(data);
+    };
+    fetchLanguage().catch(err => console.error);
   }, []);
   return (
-    <div className="flex items-center justify-between text-white bg-editor-background px-4 py-2 border-b border-b-gray-600">
-      <label className="text-sm"> main.js </label>
+    <div className="flex items-center justify-end text-white bg-editor-background px-4 py-2 border-b border-b-gray-600">
       <select onChange={e => setLanguageId(e.target.value)} className="block appearance-none bg-gray-500 py-1.5 px-6 text-sm rounded leading-tight focus:outline-none" id="grid-state">
         {languages.map(lang => snippetLanguage && snippetLanguage.includes(lang.id.toString()) && <option key={lang.id} value={lang.id}>{lang.name}</option>)}
       </select>
