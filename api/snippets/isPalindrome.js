a4e2a33037c901cb947bd58e2f8649e3d1cd81c5
Tomas Babjak <tomibabjak@gmail.com>
2022-04-26 18:32:53 +0200

    Update code snippet solutions

--- api/snippets/isPalindrome.js
+++ api/snippets/isPalindrome.js
@@ -1,8 +1,9 @@
 function isPalindrom (str) {
-  //YOUR CODE ANSWER
+  return str == str.split('').reverse().join('') ? "Palindrome!" : "Not a Palindrome!";
 }
 
 const fs = require('fs');
 const stdinBuffer = fs.readFileSync(0);
 const str = stdinBuffer.toString();
-console.log(isPalindrom(str));"
+
+console.log(isPalindrom(str));
