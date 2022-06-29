5170e67c40f261be6833553509a982776f84f877
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-06-29 18:14:07 +0430

    Fix php language issue

--- client/components/code-mirror/code-mirror.component.jsx
+++ client/components/code-mirror/code-mirror.component.jsx
@@ -1,10 +1,22 @@
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { php, phpLanguage } from "@codemirror/lang-php";
 import { EditorView } from "@codemirror/view";
-export default ({ changeHandler, ...otherProps }) => (
+import { useSelector } from 'react-redux';
+import { selectLanguageId } from '../../state/languageid/language_id.reselector';
+
+export default ({ changeHandler, ...otherProps }) => {
+  let extension = javascript({ jsx: true });
+
+  const { languageId } = useSelector(selectLanguageId);
+  if (languageId === '68') {
+
+    extension = php({baseLanguage:phpLanguage});
+  }
+  return (
     <CodeMirror
       onChange={changeHandler}
       {...otherProps}
-      extensions={[javascript({ jsx: true }), EditorView.lineWrapping]}
+      extensions={[extension, EditorView.lineWrapping]}
     />
-)
+)}
