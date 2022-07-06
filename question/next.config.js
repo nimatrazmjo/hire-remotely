701c2d32d61a13e190ccbea87aa8ada28003f899
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-07-06 23:14:27 +0430

    Add tailwind css to question service

--- question/next.config.js
+++ question/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const { join } = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  tailwindcss: {
+    config: join(__dirname, 'tailwind.config.js'),
+  },
 }
 
 module.exports = nextConfig
---
+++ question/tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+ const { join } = require('path');
+module.exports = {
+  content: [
+      join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1140px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      },
+      colors: {
+        'editor-background': '#282c34',
+        'primary': '#171C45',
+        'danger': '#FF6347',
+        'main-blue': '#0153CC',
+        'light-blue':'#1FB9FF',
+        'light-blue2':'#EAEEF5',
+        'main-gray': '#93A4C3',
+        'main-green': '#3AE794',
+        'main-orange': '#E78F35',
+      },
+    },
+  },
+  variants: {
+    opacity: ({ after }) => after(['disabled'])
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+  ],
+}
