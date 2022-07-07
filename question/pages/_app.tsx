73a0e9f118ee8946407ef531fc25dec5645bf6b6
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-07-07 15:42:30 +0430

    Add question list

--- question/pages/_app.tsx
+++ question/pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <div className='min-h-full grid bg-light-blue2 '>
+      <Component {...pageProps} />
+      </div>
+  );
 }
 
 export default MyApp
--- question/pages/index.tsx
+++ question/pages/index.tsx
@@ -2,10 +2,67 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div >
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='max-w-7xl mx-auto min-h-screen min-w-full'>
+      <div className=' bg-white rounded-xl shadow-xl my-10 px-10 py-20 grid grid-cols-[1fr_10rem] grid-rows-2  gap-y-4 items-center'>
+        <h2 className='font-medium '>Coding Question</h2>
+        <button className='text-main-blue rounded-lg bg-slate-200 px-4 py-5 self-end'> New Questions</button>
+        <table className="border col-span-2">
+                <thead className="border-b bg-slate-200 font-light py-10">
+                    <tr>
+                        <th className="text-sm font-medium text-slate-500 px-6 py-4 text-left">Name</th>
+                        <th className="text-sm font-medium text-slate-500 px-6 py-4 text-left">Languages</th>
+                        <th className="text-sm font-medium text-slate-500 px-6 py-4 text-left">Action</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-white">
+
+                    <tr className="hover:bg-slate-200">
+                        <td className="text-sm font-medium text-slate-500 px-6 py-4 text-left">
+                            <a href="#" className="text-main-blue hover:text-main-blue-dark">
+                                <span className="text-sm">Question 1</span>
+                            </a>
+                        </td>
+                        <td className="text-sm font-medium text-slate-500 px-6 py-4 text-left">
+                            <span className="text-sm">JavaScript (Node.js 12.14.0),  PHP (7.4.1),  Python (2.7.17),  Java (OpenJDK 13.0.1)</span>
+                        </td>
+                        <td className="text-sm font-medium text-slate-500 px-6 py-4 text-left">
+
+                            <a href="#" className="text-main-blue hover:text-main-blue-dark mx-1">
+
+                                <span className="text-sm">Edit</span>
+                            </a>
+                            <a href="#" className="text-main-blue hover:text-main-blue-dark mx-1">
+
+                                <span className="text-sm">Delete</span>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr className="hover:bg-slate-200">
+
+                        <td className="text-sm font-medium text-slate-500 px-6 py-4 text-left">
+
+                            <a href="#" className="text-main-blue hover:text-main-blue-dark">
+                                <span className="text-sm">Question 2</span>
+                            </a>
+                        </td>
+                        <td className="text-sm font-medium text-slate-500 px-6 py-4 text-left">
+                            <span className="text-sm">JavaScript (Node.js 12.14.0),  PHP (7.4.1),  Python (2.7.17),  Java (OpenJDK 13.0.1)</span>
+                        </td>
+                        <td className="text-sm font-medium text-slate-500 px-6 py-4 text-left">
+
+                            <a href="#" className="text-main-blue hover:text-main-blue-dark mx-1">
+
+                                <span className="text-sm">Edit</span>
+                            </a>
+                            <a href="#" className="text-main-blue hover:text-main-blue-dark mx-1">
+
+                                <span className="text-sm">Delete</span>
+                            </a>
+                        </td>
+                    </tr>
+                  </tbody>
+            </table>
+      </div>
     </div>
   )
 }
